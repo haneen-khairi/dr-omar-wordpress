e08ce7c05b0c380c63b0c883eedbf6ae29c9cc2f
Haneen Khairi <77403713+haneen-khairi@users.noreply.github.com>
2025-01-04 22:25:08 +0300

    update

--- wp-content/themes/hello-elementor/template-parts/template-home.php
+++ wp-content/themes/hello-elementor/template-parts/template-home.php
@@ -5,7 +5,7 @@
  * Description: A custom template for the home page with ACF integration.
  */
 ?>
-<?php // include get_template_part('template-parts/acf-template/custom-header');
+<?php include get_template_part('template-parts/acf-template/custom-header');
 ?>
 
 <main id="page-home" class="home">
@@ -387,5 +387,5 @@
    </section>
 </main>
 
-<?php // include get_template_part('template-parts/acf-template/custom-footer'); 
+<?php include get_template_part('template-parts/acf-template/custom-footer');
 ?>
