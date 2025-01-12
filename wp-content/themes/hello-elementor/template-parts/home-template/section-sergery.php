0cc4fbac7536c9e33b3e102e10370c94bdbd001d
Haneen Khairi <77403713+haneen-khairi@users.noreply.github.com>
2025-01-12 17:27:36 +0300

    up

--- wp-content/themes/hello-elementor/template-parts/home-template/section-sergery.php
+++ wp-content/themes/hello-elementor/template-parts/home-template/section-sergery.php
@@ -111,30 +111,30 @@ if ($surgeries_section): ?>
                 <?php
                 $surgery_group_key = "surgeries_4";
                 if (!empty($surgeries_section[$surgery_group_key])):
-                    $surgery = $surgeries_section[$surgery_group_key];
-                    $image = $surgery['surgery_image'] ?? null;
-                    $subtitle = $surgery['surgery_subtitle'] ?? '';
-                    $button_text = $surgery['surgery_button_text'] ?? 'Discover';
-                    $button_link = $surgery['surgery_button_link'] ?? '#';
+                    $surgery4 = $surgeries_section[$surgery_group_key];
+                    $image4 = $surgery4['surgery_image'] ?? null;
+                    $subtitle4 = $surgery4['surgery_subtitle'] ?? '';
+                    $button_text4 = $surgery4['surgery_button_text'] ?? 'Discover';
+                    $button_link4 = $surgery4['surgery_button_link'] ?? '#';
                 ?>
                     <div class="col-md-6 mb-md">
-                        <a class="home__surgery__page d-b p-r o-h" href="<?php echo esc_url($button_link); ?>" target="_blank">
+                        <a class="home__surgery__page d-b p-r o-h" href="<?php echo esc_url($button_link4); ?>" target="_blank">
                             <div id="img-<?php echo esc_attr(uniqid()); ?>" class="lazy-image home__surgery__img" style="padding-bottom: 160%">
                                 <img
                                     src="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 814 1300'%3E%3C/svg%3E"
-                                    data-src="<?php echo esc_url($image['url'] ?? ''); ?>"
-                                    data-srcset="<?php echo esc_url($image['url'] ?? ''); ?> 814w"
+                                    data-src="<?php echo esc_url($image4['url'] ?? ''); ?>"
+                                    data-srcset="<?php echo esc_url($image4['url'] ?? ''); ?> 814w"
                                     sizes="(max-width: 639px) 407px, 814px"
-                                    alt="<?php echo esc_attr($image['alt'] ?? 'Surgery Image'); ?>"
+                                    alt="<?php echo esc_attr($image4['alt'] ?? 'Surgery Image'); ?>"
                                     class="lazy-image__img" />
                             </div>
                             <div class="home__surgery__metas p-a w-100">
-                                <?php if (!empty($subtitle)): ?>
+                                <?php if (!empty($subtitle4)): ?>
                                     <h3 class="home__surgery__subtitle title-xxl mb-md">
-                                        <?php echo esc_html($subtitle); ?>
+                                        <?php echo esc_html($subtitle4); ?>
                                     </h3>
                                 <?php endif; ?>
-                                <span class="home__surgery__link btn"><?php echo esc_html($button_text); ?></span>
+                                <span class="home__surgery__link btn"><?php echo esc_html($button_text4); ?></span>
                             </div>
                         </a>
                     </div>
