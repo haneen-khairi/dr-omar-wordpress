8de934a95ef9565120d971515f47d907b363aeef
Haneen Khairi <77403713+haneen-khairi@users.noreply.github.com>
2025-01-12 17:18:15 +0300

    update

--- wp-content/themes/hello-elementor/template-parts/home-template/section-sergery.php
+++ wp-content/themes/hello-elementor/template-parts/home-template/section-sergery.php
@@ -20,25 +20,26 @@ if ($surgeries_section): ?>
 
                         // Extract image data safely
                         $image = $surgery_group['surgery_image'] ?? null;
+
+                        // Skip to the next iteration if the image is not valid
+                        if (!is_array($image) || empty($image['url'])) continue;
                 ?>
                         <div class="col-md-6 mb-md">
                             <a class="home__surgery__page d-b p-r o-h"
                                 target="_blank"
                                 href="<?php echo esc_url($surgery_group['surgery_button_link'] ?? '#'); ?>">
                                 <div class="lazy-image home__surgery__img" style="padding-bottom: 160%">
-                                    <?php if (is_array($image) && !empty($image['url'])): ?>
-                                        <img
-                                            src="<?php echo esc_url($image['url']); ?>"
-                                            data-src="<?php echo esc_url($image['url']); ?>"
-                                            data-srcset="<?php echo esc_url($image['url']); ?> 407w, <?php echo esc_url($image['url']); ?> 814w"
-                                            sizes="(max-width: 639px) 407px, 814px"
-                                            width="814"
-                                            height="1300"
-                                            alt="<?php echo esc_attr($image['alt'] ?? 'Surgery Image'); ?>"
-                                            class="lazy-image__img entered loaded"
-                                            data-ll-status="loaded"
-                                            srcset="<?php echo esc_url($image['url']); ?> 407w, <?php echo esc_url($image['url']); ?> 814w" />
-                                    <?php endif; ?>
+                                    <img
+                                        src="<?php echo esc_url($image['url']); ?>"
+                                        data-src="<?php echo esc_url($image['url']); ?>"
+                                        data-srcset="<?php echo esc_url($image['url']); ?> 407w, <?php echo esc_url($image['url']); ?> 814w"
+                                        sizes="(max-width: 639px) 407px, 814px"
+                                        width="814"
+                                        height="1300"
+                                        alt="<?php echo esc_attr($image['alt'] ?? 'Surgery Image'); ?>"
+                                        class="lazy-image__img entered loaded"
+                                        data-ll-status="loaded"
+                                        srcset="<?php echo esc_url($image['url']); ?> 407w, <?php echo esc_url($image['url']); ?> 814w" />
                                 </div>
                                 <div class="home__surgery__metas p-a w-100">
                                     <?php if (!empty($surgery_group['surgery_subtitle'])): ?>
