084d7acd97bc93f93a8824deec5e1cfc58e97330
Haneen Khairi <77403713+haneen-khairi@users.noreply.github.com>
2025-01-25 16:00:55 +0300

    update

--- wp-content/themes/hello-elementor/template-parts/home-template/section-medecine.php
+++ wp-content/themes/hello-elementor/template-parts/home-template/section-medecine.php
@@ -1,132 +1,55 @@
 <section class="home__medecine p-r o-h" data-kira-timeline>
     <div class="container ta-c">
         <h2 class="home__medecine__title title-xxl tt-u mb-md md-up-mb-xl">
-            Home Page
+            <?php the_field('title'); ?>
         </h2>
     </div>
     <div class="home__medecine__marquee home__medecine__marquee--top marquee">
         <div class="marquee__inner d-f ai-center">
-            <div class="marquee__sentence">
-                <span class="marquee__word">Wrinkles</span>
-                <span class="marquee__word">skin quality</span>
-                <span class="marquee__word">Hyaluronic acid</span>
-                <span class="marquee__word">Botulinum toxin</span>
-                <span class="marquee__word">Mesotherapy</span>
-                <span class="marquee__word">Peeling</span>
-            </div>
-            <div class="marquee__sentence">
-                <span class="marquee__word">Wrinkles</span>
-                <span class="marquee__word">skin quality</span>
-                <span class="marquee__word">Hyaluronic acid</span>
-                <span class="marquee__word">Botulinum toxin</span>
-                <span class="marquee__word">Mesotherapy</span>
-                <span class="marquee__word">Peeling</span>
-            </div>
-            <div class="marquee__sentence">
-                <span class="marquee__word">Wrinkles</span>
-                <span class="marquee__word">skin quality</span>
-                <span class="marquee__word">Hyaluronic acid</span>
-                <span class="marquee__word">Botulinum toxin</span>
-                <span class="marquee__word">Mesotherapy</span>
-                <span class="marquee__word">Peeling</span>
-            </div>
-            <div class="marquee__sentence">
-                <span class="marquee__word">Wrinkles</span>
-                <span class="marquee__word">skin quality</span>
-                <span class="marquee__word">Hyaluronic acid</span>
-                <span class="marquee__word">Botulinum toxin</span>
-                <span class="marquee__word">Mesotherapy</span>
-                <span class="marquee__word">Peeling</span>
-            </div>
-            <div class="marquee__sentence">
-                <span class="marquee__word">Wrinkles</span>
-                <span class="marquee__word">skin quality</span>
-                <span class="marquee__word">Hyaluronic acid</span>
-                <span class="marquee__word">Botulinum toxin</span>
-                <span class="marquee__word">Mesotherapy</span>
-                <span class="marquee__word">Peeling</span>
-            </div>
-            <div class="marquee__sentence">
-                <span class="marquee__word">Wrinkles</span>
-                <span class="marquee__word">skin quality</span>
-                <span class="marquee__word">Hyaluronic acid</span>
-                <span class="marquee__word">Botulinum toxin</span>
-                <span class="marquee__word">Mesotherapy</span>
-                <span class="marquee__word">Peeling</span>
-            </div>
+            <?php if( have_rows('repeater_sentences') ): ?>
+                <?php while( have_rows('repeater_sentences') ): the_row(); ?>
+                    <div class="marquee__sentence">
+                        <?php if( have_rows('repeater_words') ): ?>
+                            <?php while( have_rows('repeater_words') ): the_row(); ?>
+                                <span class="marquee__word"><?php the_sub_field('word'); ?></span>
+                            <?php endwhile; ?>
+                        <?php endif; ?>
+                    </div>
+                <?php endwhile; ?>
+            <?php endif; ?>
         </div>
     </div>
     <div class="home__medecine__top container">
         <div class="row h-100 no-gutters">
             <div class="home__medecine__pushTop home__medecine__pushTop--left col-md-12 col-xl-9 bg-beige p-r">
                 <div class="home__medecine__pushTopInner d-f fd-column h-100 p-r">
-                    <h3 class="title-xl tt-u">Injection of hyaluronic acid</h3>
+                    <h3 class="title-xl tt-u"><?php the_field('left_title'); ?></h3>
                     <div class="mt-auto">
-                        <a target="_blank" href="#" class="btn">Discover</a>
+                        <a target="_blank" href="<?php the_field('leftlink'); ?>" class="btn">Discover</a>
                     </div>
                 </div>
             </div>
             <div class="home__medecine__img col-md-8 md-up-d-none xl-up-d-b o-h p-r">
-                <div class="bg-cover absolute-full" data-kira-item="zoomOut"
-                    data-bg="assets/img/homepage/homepage6.jpg"></div>
+                <div class="bg-cover absolute-full" data-kira-item="zoomOut" data-bg="assets/img/homepage/homepage6.jpg"></div>
             </div>
-            <div
-                class="home__medecine__pushTop home__medecine__pushTop--right col-md-12 col-xl-7 h-100 bg-black c-white p-r">
-                <div class="bg-cover absolute-full" data-kira-item="zoomOut"
-                    data-bg="assets/img/homepage/homepage6.jpg"></div>
+            <div class="home__medecine__pushTop home__medecine__pushTop--right col-md-12 col-xl-7 h-100 bg-black c-white p-r">
+                <div class="bg-cover absolute-full" data-kira-item="zoomOut" data-bg="assets/img/homepage/homepage6.jpg"></div>
             </div>
         </div>
     </div>
     <div class="home__medecine__marquee home__medecine__marquee--bottom marquee" data-direction="1">
         <div class="marquee__inner d-f ai-center">
-            <div class="marquee__sentence">
-                <span class="marquee__word">Peeling</span>
-                <span class="marquee__word">skin quality</span>
-                <span class="marquee__word">Mesotherapy</span>
-                <span class="marquee__word">Botulinum toxin</span>
-                <span class="marquee__word">Wrinkles</span>
-                <span class="marquee__word">Hyaluronic acid</span>
-            </div>
-            <div class="marquee__sentence">
-                <span class="marquee__word">Peeling</span>
-                <span class="marquee__word">skin quality</span>
-                <span class="marquee__word">Mesotherapy</span>
-                <span class="marquee__word">Botulinum toxin</span>
-                <span class="marquee__word">Wrinkles</span>
-                <span class="marquee__word">Hyaluronic acid</span>
-            </div>
-            <div class="marquee__sentence">
-                <span class="marquee__word">Peeling</span>
-                <span class="marquee__word">skin quality</span>
-                <span class="marquee__word">Mesotherapy</span>
-                <span class="marquee__word">Botulinum toxin</span>
-                <span class="marquee__word">Wrinkles</span>
-                <span class="marquee__word">Hyaluronic acid</span>
-            </div>
-            <div class="marquee__sentence">
-                <span class="marquee__word">Peeling</span>
-                <span class="marquee__word">skin quality</span>
-                <span class="marquee__word">Mesotherapy</span>
-                <span class="marquee__word">Botulinum toxin</span>
-                <span class="marquee__word">Wrinkles</span>
-                <span class="marquee__word">Hyaluronic acid</span>
-            </div>
-            <div class="marquee__sentence">
-                <span class="marquee__word">Peeling</span>
-                <span class="marquee__word">skin quality</span>
-                <span class="marquee__word">Mesotherapy</span>
-                <span class="marquee__word">Botulinum toxin</span>
-                <span class="marquee__word">Wrinkles</span>
-                <span class="marquee__word">Hyaluronic acid</span>
-            </div>
-            <div class="marquee__sentence">
-                <span class="marquee__word">Peeling</span>
-                <span class="marquee__word">skin quality</span>
-                <span class="marquee__word">Mesotherapy</span>
-                <span class="marquee__word">Botulinum toxin</span>
-                <span class="marquee__word">Wrinkles</span>
-                <span class="marquee__word">Hyaluronic acid</span>
-            </div>
+            <?php if( have_rows('repeater_sentences') ): ?>
+                <?php while( have_rows('repeater_sentences') ): the_row(); ?>
+                    <div class="marquee__sentence">
+                        <?php if( have_rows('repeater_words') ): ?>
+                            <?php while( have_rows('repeater_words') ): the_row(); ?>
+                                <span class="marquee__word"><?php the_sub_field('word'); ?></span>
+                            <?php endwhile; ?>
+                        <?php endif; ?>
+                    </div>
+                <?php endwhile; ?>
+            <?php endif; ?>
         </div>
     </div>
     <div class="home__medecine__bottom container p-r">
@@ -141,8 +64,7 @@
             </div>
             <div class="col-right p-0 p-r">
                 <div class="home__medecine__pushBottomImg p-r o-h">
-                    <div class="bg-cover absolute-full" data-kira-item="zoomOut"
-                        data-bg="assets/img/homepage/homepage7.jpg"></div>
+                    <div class="bg-cover absolute-full" data-kira-item="zoomOut" data-bg="assets/img/homepage/homepage7.jpg"></div>
                 </div>
             </div>
         </div>
@@ -157,8 +79,7 @@
             </div>
             <div class="col-right p-0 p-r">
                 <div class="home__medecine__pushBottomImg p-r o-h">
-                    <div class="bg-cover absolute-full" data-kira-item="zoomOut"
-                        data-bg="assets/img/homepage/homepage8.jpg"></div>
+                    <div class="bg-cover absolute-full" data-kira-item="zoomOut" data-bg="assets/img/homepage/homepage8.jpg"></div>
                 </div>
             </div>
         </div>
